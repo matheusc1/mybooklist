142810c1b07a3915b47b9bbe1ba0e08aba7014ec
Matheus Cardoso <cardoso.matheusbs@gmail.com>
2026-05-28 15:58:50 -0300

    refactor: extract User and Goal types to types file

--- src/types/types.ts
+++ src/types/types.ts
@@ -4,3 +4,15 @@ export type ActivityStatus =
 	| 'paused'
 	| 'abandoned'
 	| 'want-to-read'
+
+export type User = {
+	name: string
+	email: string
+	avatar?: string
+}
+
+export type Goal = {
+	target: number
+	current: number
+	year: number
+}
